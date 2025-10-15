c396d1d997bfb5c54dbb9b78c4fcb48ca72f9862
Aparna Singh <aparnasingh@Aparnas-MacBook-Pro.local>
2025-10-15 22:29:38 +0530

    Add hello world script in COBOL by tinkertechie

---
+++ scripts/hello_world_tinkertechie.cob
@@ -0,0 +1,10 @@
+      * LANGUAGE: COBOL
+      * ENV: GnuCOBOL or Mainframe
+      * AUTHOR: Tinker Techie
+      * GITHUB: https://github.com/tinkertechie
+
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO-TINKERTECHIE.
+       PROCEDURE DIVISION.
+           DISPLAY "Hello, World!".
+           STOP RUN.
